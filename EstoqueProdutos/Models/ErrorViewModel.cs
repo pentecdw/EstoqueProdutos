1583106c793ac86356a00780c192ec2668b788b6
pentecdw <carloseduardof981@gmail.com>
2025-06-25 22:29:13 -0300

    COMMIT DO MIGRATION E CRIANDO OPERAÇÕES DE CRUD

---
+++ EstoqueProdutos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EstoqueProdutos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
